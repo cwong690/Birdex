06f885ad703ceb72684bc6c3592c12784886c05d
cwong690 <cwong690@gmail.com>
2020-06-28 06:31:19 +0000

    fix import werkzeug.utils

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import pandas as pd
 import numpy as np
 import os
-from werkzeug import secure_filename
+from werkzeug.utils import secure_filename
 
 from PIL import Image
 import requests
